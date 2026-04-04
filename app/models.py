b38e08864119f9882b25ac074449112c29a43c72
Indranil102e <indranilsamanta2003@gmail.com>
2026-04-04 16:57:00 +0530

    ccreate a aclass of records

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column,Integer, String,Boolean
+from sqlalchemy import Column,Integer, String,Boolean, Float,Date,ForeignKey
 from app.database import Base
+from sqlalchemy.orm import relationship
 
 class User(Base):
     __tablename__="users"
@@ -10,3 +11,17 @@ class User(Base):
     password=Column(String,nullable=False)
     role=Column(String,default="viewer")
     is_active=Column(Boolean,default=True)
+    
+    
+class Record(Base):
+    __tablename__="records"
+    
+    id=Column(Integer,primary_key=True,index=True)
+    amount=Column(Float,nullable=False)
+    type=Column(String,nullable=False)
+    category=Column(String,nullable=False)
+    date=Column(Date)
+    note=Column(String)
+    
+    created_by=Column(Integer,ForeignKey("users.id"))
+    owner=relationship("User")
