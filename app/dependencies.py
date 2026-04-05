7486be7a15a1ffc3f63cdc8936f0c26495585103
Indranil102e <indranilsamanta2003@gmail.com>
2026-04-05 17:15:29 +0530

    Enhance authentication flow: implement user role verification and improve login endpoint with OAuth2PasswordRequestForm

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,10 @@
 from app.database import SessionLocal
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from app.auth import SECRET_KEY, ALGORITHM
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 def get_db():
     db = SessionLocal()
@@ -7,3 +12,24 @@ def get_db():
         yield db
     finally:
         db.close()
+        
+        
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload=jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email=payload.get("sub")
+        role=payload.get("role")
+        
+        if email is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        return {"email": email, "role": role}
+    
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    
+def require_role(required_role:str):
+    def role_checker(user=Depends(get_current_user)):
+        if user["role"]!= required_role:
+            raise HTTPException(status_code=403, detail="Permission denied")
+        return user
+    return role_checker
--- app/routes/users.py
+++ app/routes/users.py
@@ -4,27 +4,37 @@ from app.auth import hash_password
 from app import models, schemas
 from app.auth import verify_password, create_access_token
 from app.dependencies import get_db
+from app.dependencies import require_role
+from fastapi.security import OAuth2PasswordRequestForm
+
 router= APIRouter()
 
 @router.post("/login")
-def login(user: schemas.LoginSchema, db: Session = Depends(get_db)):
-    
-    db_user= db.query(models.User).filter(models.User.email==user.email).first()
-    
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
+    db_user = db.query(models.User).filter(
+        models.User.email == form_data.username
+    ).first()
+
     if not db_user:
-        raise HTTPException(status_code=404, detail="User not found ")
-    if not verify_password(user.password, db_user.password):
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if not verify_password(form_data.password, db_user.password):
         raise HTTPException(status_code=401, detail="Incorrect password")
-    token=create_access_token(
-        {"sub": db_user.email,"role": db_user.role}
+
+    token = create_access_token(
+        {"sub": db_user.email, "role": db_user.role}
     )
-    
+
     return {"access_token": token, "token_type": "bearer"}
 
 
-
 @router.post("/users")
-def create_user(user:schemas.UserCreate, db: Session= Depends(get_db)):
+def create_user( user: schemas.UserCreate,
+    db: Session = Depends(get_db),
+    admin=Depends(require_role("admin"))):
     
     existing_user= db.query(models.User).filter(models.User.email==user.email).first()
     
