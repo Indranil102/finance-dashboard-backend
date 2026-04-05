e06b65e2672e6a0609bb96164dc29352ae55d1e1
Indranil102e <indranilsamanta2003@gmail.com>
2026-04-05 16:44:17 +0530

    Fix create_access_token function: correct datetime typo and improve formatting

--- app/auth.py
+++ app/auth.py
@@ -14,10 +14,11 @@ def hash_password(password: str):
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
 
-def create_access_token(data:dict):
-    to_encode=data.copy()
-    
-    expire= datatime.utcow()+timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp":expire})
-    
-    return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
