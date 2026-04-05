2cc6740d04a1eb635ff48bd8fba985836fb8cf78
Indranil102e <indranilsamanta2003@gmail.com>
2026-04-05 18:32:02 +0530

    Refactor main application structure: organize imports, add main execution block, and create railway configuration for deployment

--- app/main.py
+++ app/main.py
@@ -2,18 +2,22 @@ from fastapi import FastAPI
 from app.database import engine, Base
 from app import models
 from app.routes import users
-
-
 from app.routes import records
-
 from app.routes import dashboard
-app = FastAPI()
+import os
 
+app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 app.include_router(dashboard.router)
 app.include_router(users.router)
 app.include_router(records.router)
+
 @app.get("/")
 def home():
     return {"message":"Finance Dashboards"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
